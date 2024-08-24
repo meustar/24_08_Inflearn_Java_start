05ff50ada6a1021dc17b3873e7b90dfb09c00c1f
meustar <meustar829@gmail.com>
2024-08-25 00:17:26 +0900

    git section.2 주석(comment)

---
+++ src/CommentJava.java
@@ -0,0 +1,17 @@
+public class CommentJava {
+    // 주석 (comment)
+    /* 소스코드가 복잡하다면 소스코드에 대한 이해를 돕기 위해 설명을 적어두거나,
+    *  특정 코드를 지우지 않고, 잠시 실행을 막아두고 싶을 때 주석을 사용한다.
+    *  자바는 주석이 있는 곳을 무시한다.
+    * */
+
+    public static void main(String[] args) {
+        System.out.println("hello java1"); // hello java1을 출력합니다.
+        //System.out.println("hello java2"); 한 줄 주석 (single line comment)
+        
+        /* 여러 줄 주석 (multi line comment)
+        System.out.println("hello java3");
+        System.out.println("hello java4");
+        */
+    }
+}
